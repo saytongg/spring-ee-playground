9e781388a8fc04e2c2d5bbed48ca52dbd9419290
saytongg <braullojo.bj@gmail.com>
2025-11-28 00:36:25 +0800

    feat: specify property file(s) to be used based on profile

--- src/main/java/com/saytongg/shared/main/MainConfiguration.java
+++ src/main/java/com/saytongg/shared/main/MainConfiguration.java
@@ -2,9 +2,11 @@ package com.saytongg.shared.main;
 
 import java.io.IOException;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
@@ -14,8 +16,43 @@ import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 public class MainConfiguration {
 
 	@Bean
+	@Profile("dev")
 	public static PropertySourcesPlaceholderConfigurer devPropertySourcesPlaceholderConfigurer() throws IOException {
-		Resource[] resourceList = new PathMatchingResourcePatternResolver().getResources("classpath:*.properties");
+		Resource[] resourceList = ArrayUtils.addAll(
+				new PathMatchingResourcePatternResolver().getResources("classpath:properties/general/*.properties"),
+				new PathMatchingResourcePatternResolver().getResources("classpath:properties/dev/*.properties"));
+
+		PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+		propertySourcesPlaceholderConfigurer.setIgnoreResourceNotFound(true);
+		propertySourcesPlaceholderConfigurer.setIgnoreUnresolvablePlaceholders(true);
+		propertySourcesPlaceholderConfigurer.setLocations(resourceList);
+		propertySourcesPlaceholderConfigurer.setLocalOverride(true);
+
+		return propertySourcesPlaceholderConfigurer;
+	}
+	
+	@Bean
+	@Profile("staging")
+	public static PropertySourcesPlaceholderConfigurer stagingPropertySourcesPlaceholderConfigurer() throws IOException {
+		Resource[] resourceList = ArrayUtils.addAll(
+				new PathMatchingResourcePatternResolver().getResources("classpath:properties/general/*.properties"),
+				new PathMatchingResourcePatternResolver().getResources("classpath:properties/staging/*.properties"));
+
+		PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+		propertySourcesPlaceholderConfigurer.setIgnoreResourceNotFound(true);
+		propertySourcesPlaceholderConfigurer.setIgnoreUnresolvablePlaceholders(true);
+		propertySourcesPlaceholderConfigurer.setLocations(resourceList);
+		propertySourcesPlaceholderConfigurer.setLocalOverride(true);
+
+		return propertySourcesPlaceholderConfigurer;
+	}
+	
+	@Bean
+	@Profile("prod")
+	public static PropertySourcesPlaceholderConfigurer prodPropertySourcesPlaceholderConfigurer() throws IOException {
+		Resource[] resourceList = ArrayUtils.addAll(
+				new PathMatchingResourcePatternResolver().getResources("classpath:properties/general/*.properties"),
+				new PathMatchingResourcePatternResolver().getResources("classpath:properties/prod/*.properties"));
 
 		PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
 		propertySourcesPlaceholderConfigurer.setIgnoreResourceNotFound(true);
